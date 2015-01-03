242e4a5701a12418e635afbf4b86dc9d3e511276
Michael Piccirilli <michael.r.piccirilli@gmail.com>
2015-01-03 12:26:08 -0500

    update auth function

--- R/INOAuth.R
+++ R/INOAuth.R
@@ -1,10 +1,10 @@
-INOAuth <- function(consumer_key, consumer_secret)
+inOAuth <- function(application_name, consumer_key, consumer_secret)
 {
   full_url <- oauth_callback()
   message <- paste("Copy and paste into 'OAuth 2.0 Redirect URLs' on LinkeIn Application Details:", full_url, "\nWhen done, press any key to continue...")
   invisible(readline(message))
-  lnkdin <- oauth_endpoint("requestToken", "authorize", "accessToken", base_url = "https://api.linkedin.com/uas/oauth/")
-  myapp <- oauth_app(appname = "mikesapp", consumer_key, consumer_secret)
+  linkedin <- oauth_endpoint("requestToken", "authorize", "accessToken", base_url = "https://api.linkedin.com/uas/oauth/")
+  myapp <- oauth_app(appname = application_name, consumer_key, consumer_secret)
   token <- oauth1.0_token(linkedin, myapp)
   return(token)
 }
