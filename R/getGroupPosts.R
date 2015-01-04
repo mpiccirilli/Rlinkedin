9d088dcfcb17e5c24646de76b85a7900a404c83e
Michael Piccirilli <michael.r.piccirilli@gmail.com>
2015-01-03 22:42:50 -0500

    add getGroup functions

---
+++ R/getGroupPosts.R
@@ -0,0 +1,71 @@
+getGroupPosts <- function(token)
+{
+  membership_url <- "https://api.linkedin.com/v1/people/"
+  membership_fields <- "/group-memberships:(group:(id,name),membership-state,show-group-logo-in-profile,allow-messages-from-members,email-digest-frequency,email-announcements-from-managers,email-for-every-new-post)"
+  
+  # group details:
+  details_url <- "https://api.linkedin.com/v1/groups/"
+  details_fields <- ":(id,name,short-description,description,relation-to-viewer:(membership-state,available-actions),posts,counts-by-category,is-open-to-non-members,category,website-url,locale,location:(country,postal-code),allow-member-invites,site-group-url,small-logo-url,large-logo-url)"
+  
+  # if memberships=TRUE, details=TRUE, posts=TRUE
+  # This will return the posts of groups you're in
+  
+  url <- paste0(membership_url,"~",membership_fields)
+  query <- GET(url, config(token=token))
+  q.content <- content(query)
+  group.ids <- unlist(xpathApply(q.content, "//group/id", xmlValue))
+  q.df <- data.frame()
+  for(i in 1:length(group.ids))
+  {
+    url <- paste0(details_url, group.ids[i], details_fields)  
+    query <- GET(url, config(token=token))
+    q.content <- content(query)
+    
+    gp.id <- q.content["number(//group/id/text())"]
+    gp.name <- q.content["string(//group/name/text())"]
+    
+    post.nodes <- getNodeSet(q.content, "//post")
+    er <- tryCatch(
+      {
+      post.id <- lapply(post.nodes, xpathSApply, "./id", xmlValue)
+      post.id[sapply(post.id, is.list)] <- NA
+      post.id <- unlist(post.id)
+      
+      poster.fname <- lapply(post.nodes, xpathSApply, "./creator/first-name", xmlValue)
+      poster.fname[sapply(poster.fname, is.list)] <- NA
+      poster.fname <- unlist(poster.fname)
+      
+      poster.lname <- lapply(post.nodes, xpathSApply, "./creator/last-name", xmlValue)
+      poster.lname[sapply(poster.lname, is.list)] <- NA
+      poster.lname <- unlist(poster.lname)
+      
+      post.headline <- lapply(post.nodes, xpathSApply, "./creator/headline", xmlValue)
+      post.headline[sapply(post.headline, is.list)] <- NA
+      post.headline <- unlist(post.headline)
+      
+      post.title <- lapply(post.nodes, xpathSApply, "./title", xmlValue)
+      post.title[sapply(post.title, is.list)] <- NA
+      post.title <- unlist(post.title)
+      
+    }, 
+    error=function(e)
+      {
+      NULL
+      })
+    if(is.null(er))
+    {
+      post.id <- NA
+      poster.fname <- NA
+      poster.lname <- NA
+      post.headline <- NA
+      post.title <- NA
+    }
+    
+    temp.df <- data.frame(group_id=gp.id, group_name=gp.name, post_id=post.id,
+                          poster_fname=poster.fname, poster_lname=poster.lname,
+                          post_headline=post.headline, post_title=post.title)
+    q.df <- rbind(q.df, temp.df)
+    }
+  return(q.df)
+}
+
---
+++ R/getGroups.R
@@ -0,0 +1,47 @@
+getGroups <- function(token, details=FALSE)
+{
+  # This function will get all the groups a user (including self) is a member of
+  membership_url <- "https://api.linkedin.com/v1/people/"
+  membership_fields <- "/group-memberships:(group:(id,name),membership-state,show-group-logo-in-profile,allow-messages-from-members,email-digest-frequency,email-announcements-from-managers,email-for-every-new-post)"
+  
+  # group details:
+  details_url <- "https://api.linkedin.com/v1/groups/"
+  details_fields <- ":(id,name,short-description,description,relation-to-viewer:(membership-state,available-actions),posts,counts-by-category,is-open-to-non-members,category,website-url,locale,location:(country,postal-code),allow-member-invites,site-group-url,small-logo-url,large-logo-url)"
+  
+  
+  # if details=FALSE
+  # This will return a list of groups you're in 
+  if(!isTRUE(details)){
+    url <- paste0(membership_url,"~",membership_fields)
+    query <- GET(url, config(token=token))
+    q.content <- content(query)
+    q.list <- xmlToList(q.content)
+    q.df <- do.call(data.frame, unlist(q.list, recursive=FALSE))
+    return(q.df)
+  }
+    
+  # if details=TRUE
+  # This will return group details of groups you're in
+  if(isTRUE(details)){
+    url <- paste0(membership_url,"~",membership_fields)
+    query <- GET(url, config(token=token))
+    q.content <- content(query)
+    group.ids <- unlist(xpathApply(q.content, "//group/id", xmlValue))
+    q.df <- data.frame()
+    for(i in 1:length(group.ids))
+    {
+      url <- paste0(details_url, group.ids[i], details_fields)  
+      query <- GET(url, config(token=token))
+      q.content <- content(query)
+      gp.id <- q.content["number(//group/id/text())"]
+      gp.name <- q.content["string(//group/name/text())"]
+      gp.s.desc <- q.content["string(//group/short-description/text())"]
+      gp.l.desc <- q.content["string(//group/description/text())"]
+      temp.df <- data.frame(group_id=gp.id, group_name=gp.name,
+                            group_desc_short=gp.s.desc, group_desc_long=gp.l.desc)
+      q.df <- rbind(q.df, temp.df)
+    }
+    return(q.df)
+  }
+}
+
