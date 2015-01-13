6256fb73c1e9eb9882208ec9790ba452ba359066
Michael Piccirilli <michael.r.piccirilli@gmail.com>
2015-01-13 10:44:03 -0500

    update searchJobs functionality, add searchPeople

--- R/searchJobs.R
+++ R/searchJobs.R
@@ -1,52 +1,64 @@
 #' Job Search API: Search for Jobs on LinkedIn
 #'
+#' The Job Search API enables search across LinkedIn's job postings.
+#' 
 #' Job Search API is a part of our Vetted API Access Program. You must apply and get LinkedIn's approval before using this API.
+#' 
+#' Throttle limits: Up to 100 returns per search, 10 returns per page. Each page is one API call. 
 #'
 #'
 #' @param token Authorization token 
-#' @param keyword Keyword job search 
-#' @param count Authorization token 
+#' @param keyword Keyword search jobs by keyword
+#' @param company_name Company search by company name
+#' @param job_title Title search by job title
+#' @param country_code  ISO3166 country code, must be in lower case
+#' @param postal_code Postal Code must combined with country-code
+#' @param distance Distance matches members within a distance from a central point. This is measured in miles. This is best used in combination with both country-code and postal-code.
 #' @return Returns jobs based input parameters
 #' @examples
 #' \dontrun{
 #' 
-#' search.results <- searchJobs(in.auth, "data scientist")
+#' search.results <- searchJobs(in.auth, keywords = "data scientist")
 #' }
 #' @export
 
 
-searchJobs <- function(token, keywords, count=20)
-{
-  # Need to include more parameters
-  
-  # In order to utilize this function you need to apply for Vetted API Access
-  # Right now I only have a search based on keywords. 
-  
-  # Count can range from 1 to 20
-  # Throttle limits: Up to 100 returns per search, 10 returns per page. Each page is one API call. 
 
-  kw.search <- URLencode(keywords)
-  start <- 0
-  base_url <- paste0("https://api.linkedin.com/v1/job-search?start=",start,"&count=",count,"&")
-  kw.url <- paste0("keywords=",kw.search)
+
+searchJobs <- function(token, keywords=NULL, company_name=NULL, job_title=NULL, country_code=NULL, postal_code=NULL, distance=NULL)
+{
+  base_url <- "https://api.linkedin.com/v1/job-search:(jobs:(id,customer-job-code,active,posting-date,expiration-date,posting-timestamp,expiration-timestamp,company:(id,name),position:(title,location,job-functions,industries,job-type,experience-level),skills-and-experience,description-snippet,description,salary,job-poster:(id,first-name,last-name,headline),referral-bonus,site-job-url,location-description))?"
   
-  url <- paste0(base_url, kw.url)
-  query <- GET(url, config(token = token))
+  kw <- if(!is.null(keywords)) URLencode(paste0("keywords=",keywords,"&"))
+  comp_name <- if(!is.null(company_name)) URLencode(paste0("company-name=",company_name,"&"))
+  jb_ttl <- if(!is.null(job_title)) URLencode(paste0("job-title=",job_title,"&"))
+  ctry_code <- if(!is.null(country_code)) URLencode(paste0("country-code=",country_code,"&")) 
+  pstl_code <- if(!is.null(postal_code)) URLencode(paste0("postal-code=",postal_code,"&"))
+  dist <- if(!is.null(distance)) URLencode(paste0("distance=",distance,"&")) # miles
+  ct <- URLencode(paste0("count=",10))
+  url <- paste0(base_url, kw, comp_name, jb_ttl, curr_sch, ctry_code, pstl_code, dist, ct)
+  query <- GET(url, config(token=token))
   q.content <- content(query)
-  search.total <- as.numeric(xmlAttrs(q.content[["//job-search/jobs[@total]"]])[[1]])
-  #The number of jobs to return. Values can range between 0 and 20. The default value is 10. The total results available to any user depends on their account level.
-  search.count <- as.numeric(xmlAttrs(q.content[["//job-search/jobs[@total]"]])[[2]])
-  total.pages <- ceiling(search.total/search.count)
-  q.df <- data.frame()
-  for(i in 1:total.pages)
-  {
-    if(i==1) start <- 0 else start <- (i-1)*search.count
-    base_url <- paste0("https://api.linkedin.com/v1/job-search?start=",start,"&count=",count,"&")
-    url <- paste0(base_url, kw.url)
-    query <- GET(url, config(token = token))
-    q.content <- content(query)
-    q.tmp <- jobsToDF(q.content)
-    q.df <- rbind(q.df, q.tmp)
+  if(!is.na(q.content[["number(//error/status)"]]==403)){
+    print(q.content[["string(//error/message)"]])
   }
-  return(q.df)
+  else {
+    p1 <- jobsToDF(q.content)
+    search.total <- as.numeric(xmlAttrs(q.content[["//job-search/jobs"]])[[1]])
+    search.count <- if(search.total<10) search.total else as.numeric(xmlAttrs(q.content[["//job-search/jobs"]])[[2]])
+    total.pages <- ceiling(search.total/search.count)
+    q.df <- data.frame()
+    for(i in 2:total.pages)
+    {
+      if(i==1) start <- 0 else start <- (i-1)*search.count
+      full_url <- paste0(base_url,kw, comp_name, jb_ttl, curr_sch, ctry_code, pstl_code, dist)
+      pages_url <- paste0(full_url, "start=",start,"&",ct)
+      query <- GET(pages_url, config(token = token))
+      q.content <- content(query)
+      q.tmp <- jobsToDF(q.content)
+      q.df <- rbind(q.df, q.tmp)
+    }
+    out <- rbind(p1,q.df)
+    return(out)
+  }
 }
---
+++ R/searchPeople.R
@@ -0,0 +1,78 @@
+#' People Search API: Search for People on LinkedIn
+#'
+#' The People Search API returns information about people. It lets you implement most of what shows up when you do a search for "People" in the top right box on LinkedIn.com.
+#' 
+#' People Search API is a part of LinkedIn's Vetted API Access Program. You must apply and get LinkedIn's approval before using this API.
+#' 
+#' Up to 100 returns per search, 10 returns per page. Each page is one API call. 
+#'
+#' @param token Authorization token 
+#' @param keywords Keyword people search 
+#' @param first_name First Name  
+#' @param last_name Last Name
+#' @param company_name Company Name 
+#' @param current_company TRUE/FALSE
+#' @param title Job Title
+#' @param current_title TRUE/FALSE
+#' @param school_name School Name of a school
+#' @param curent_school TRUE/FALSE 
+#' @param country_code TRUE/FALSE 
+#' @param postal_code Code Must be combined with the country-code parameter.
+#' @param distanace Distance Matches members within a distance from a central point. This is measured in miles.
+#'
+#' @return Returns people search based input parameters
+#' @examples
+#' \dontrun{
+#' 
+#' search.results <- searchPeople(token=in.auth, first_name="Michael", last_name="Piccirilli")
+#' }
+#' @export
+
+
+searchPeople <- function(token, keywords=NULL, first_name=NULL, last_name=NULL, company_name=NULL, current_company=NULL, title=NULL, current_title=NULL, school_name=NULL, current_school=NULL, country_code=NULL, postal_code=NULL, distance=NULL)
+{
+  base_url <- "https://api.linkedin.com/v1/people-search:(people:(id,first-name,last-name,formatted-name,location:(name),headline,industry,num-connections,summary,specialties,positions))?"
+
+  # Should build a nicer way of doing this...
+  kw <- if(!is.null(keywords)) URLencode(paste0("keywords=",keywords,"&"))
+  fname <- if(!is.null(first_name)) URLencode(paste0("first-name=",first_name,"&"))
+  lname <- if(!is.null(last_name)) URLencode(paste0("last-name=",last_name,"&"))
+  comp <- if(!is.null(company_name)) URLencode(paste0("company-name=",company_name,"&"))
+  curr_comp <- if(!is.null(current_company)) URLencode(paste0("current-company=",current_company,"&")) # true/false
+  ttl <- if(!is.null(title)) URLencode(paste0("title=",title,"&"))
+  curr_ttl <- if(!is.null(current_title)) URLencode(paste0("current-title=",current_title,"&")) # true/false
+  sch_name <- if(!is.null(school_name)) URLencode(paste0("school-name=",school_name,"&"))
+  curr_sch <- if(!is.null(current_school)) URLencode(paste0("current-school=",current_school,"&")) # true/false
+  ctry_code <- if(!is.null(country_code)) URLencode(paste0("country-code=",country_code,"&")) 
+  pstl_code <- if(!is.null(postal_code)) URLencode(paste0("postal-code=",postal_code,"&"))
+  dist <- if(!is.null(distance)) URLencode(paste0("distance=",distance,"&")) # miles
+  ct <- URLencode(paste0("count=",10))
+  
+  url <- paste0(base_url, kw, fname, lname, comp, curr_comp, ttl, curr_ttl, sch_name,
+               curr_sch, ctry_code, pstl_code,dist,ct)
+  query <- GET(url, config(token=token))
+  q.content <- content(query)
+  if(!is.na(q.content[["number(//error/status)"]]==403)){
+    print(q.content[["string(//error/message)"]])
+  }
+  else {
+    p1 <- searchPeopleToDF(q.content)
+    search.total <- as.numeric(xmlAttrs(q.content[["//people-search/people"]])[[1]])
+    search.count <- if(search.total<10) search.total else as.numeric(xmlAttrs(q.content[["//people-search/people"]])[[2]])
+    total.pages <- ceiling(search.total/search.count)
+    q.df <- data.frame()
+    for(i in 2:total.pages)
+    { 
+      if(i==1) start <- 0 else start <- (i-1)*search.count
+      full_url <- paste0(base_url, kw, fname, lname, comp, curr_comp, ttl, curr_ttl, sch_name,
+                    curr_sch, ctry_code, pstl_code, dist)
+      pages_url <- paste0(full_url, "start=",start,"&",ct)
+      query <- GET(pages_url, config(token = token))
+      q.content <- content(query)
+      q.tmp <- searchPeopleToDF(q.content)
+      q.df <- rbind(q.df, q.tmp)
+    }
+    out <- rbind(p1,q.df)
+    return(out)
+  }
+}
