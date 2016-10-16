d4f169c8495f3eb844cdbf1025897da967ead0d6
Michael Piccirilli <michael.r.piccirilli@gmail.com>
2016-10-16 13:08:01 -0700

    Updating default credentials

--- R/inOAuth.R
+++ R/inOAuth.R
@@ -38,16 +38,27 @@
 
 inOAuth <- function(application_name=NULL, consumer_key=NULL, consumer_secret=NULL)
 {
+  
+  install.packages('XML')
+  install.packages('httpuv')
+  
+  
+  library(httr)
+  library(httpuv)
+  
+  
   full_url <- oauth_callback()
   message <- paste("If you've created you're own application, be sure to copy and paste the following into \n 'OAuth 2.0 Redirect URLs' in the LinkedIn Application Details:", full_url, "\n When done, press any key to continue...")
   invisible(readline(message))
   
   if(is.null(application_name) && is.null(consumer_key) && is.null(consumer_secret)){
-    application_name <- "Rlinkedin"
-    consumer_key <- "77w6b6fmmu2967"
-    consumer_secret <- "JcHFtrazNQAgBVMx"
+    application_name <- "RLnkdInV2"
+    consumer_key <- "75yrky9cluyq7b"
+    consumer_secret <- "ZRZ9Wm6mgyYvUiXL"
     linkedin <- oauth_endpoint("requestToken", "authorize", "accessToken",
                                base_url = "https://api.linkedin.com/uas/oauth")
+    
+
     myapp <- oauth_app(appname = application_name, consumer_key, consumer_secret)
     token <- oauth1.0_token(linkedin, myapp)
     return(token)
